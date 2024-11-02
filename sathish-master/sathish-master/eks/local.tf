2120f918cbf42eee48e905f7926b9c7326530998
venkatesh <venky03052003@gmail.com>
2024-11-02 17:37:35 +0530

    csv

---
+++ sathish-master/sathish-master/eks/local.tf
@@ -0,0 +1,25 @@
+/*locals {
+worker_groups = [
+    {
+      name                          = "frontend-worker-group"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      asg_desired_capacity          = 2
+    },
+    {
+      name                          = "backend-java-worker-group"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+   {
+      name                          = "database-worker-group"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    }, 
+  ]
+}*/
